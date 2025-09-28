1709951cc270b8d50167f4103fcb91509ac06d8f
stiletamz-lgtm <stiletamz@gmail.com>
2025-09-27 23:14:52 -0400

    Update Query.sql

--- Azure Synapse Analytics/Query.sql
+++ Azure Synapse Analytics/Query.sql
@@ -1,1 +1,32 @@
+create or ALTER view dbo.badalerts
+as
+select * FROM
+    OPENROWSET (
+        bulk 'https://adlslivedata2025.dfs.core.windows.net/azure-webjobs-eventhub/iot/badalerts/*.parquet',
+        format = 'parquet'
+    ) as [result]
+
+
+
+
+
+create or ALTER view dbo.goodevents
+as
+select * FROM
+    OPENROWSET (
+        bulk 'https://adlslivedata2025.dfs.core.windows.net/azure-webjobs-eventhub/iot/iotdata/*.parquet',
+        format = 'parquet'
+    ) as [result]
+
+
+
+
+create or ALTER view dbo.driversbeltsalert
+as
+select * FROM
+    OPENROWSET (
+        bulk 'https://adlslivedata2025.dfs.core.windows.net/azure-webjobs-eventhub/iot/belts/DriverBelts/*.parquet',
+        format = 'parquet'
+    ) as [result]
+
 
