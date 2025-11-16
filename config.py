e25a36a0872ce03794c131483b750d489baf82c0
Mar8el <stiletamz@gmail.com>
2025-11-15 19:16:46 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Hardcoded credentials - REPLACE with your actual values!
+AZURE_TENANT_ID="---"
+AZURE_CLIENT_ID="---"
+AZURE_CLIENT_SECRET="---"
+
+EVENT_HUB_CONNECTION_STR = "Endpoint=sb://---"
+EVENT_HUB_NAME = "---"
