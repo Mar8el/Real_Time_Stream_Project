e2c5b662d35a198642c92cbb689caeb92f2e61b6
Mar8el <stiletamz@gmail.com>
2025-11-15 19:27:45 -0500

    Update Function.py

--- Azure Function/Function.py
+++ Azure Function/Function.py
@@ -1,1 +1,14 @@
+import logging
+import json
+import uuid
+import azure.functions as func
+from azure.cosmos import CosmosClient
+from azure.eventhub import EventHubProducerClient, EventData
 
+COSMOS_URL = "--"
+COSMOS_KEY = "--"
+DATABASE_NAME = "--" 
+CONTAINER_NAME = "--"
+
+cosmos_client = CosmosClient(COSMOS_URL, COSMOS_KEY)
+database = 
