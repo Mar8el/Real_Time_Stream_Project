d1405bbde9dcf259cea73fd85c3bb85bdf49364b
Mar8el <stiletamz@gmail.com>
2025-11-27 12:03:16 -0500

    Update Job_query.sql

--- Azure Stream Analytics/Job_query.sql
+++ Azure Stream Analytics/Job_query.sql
@@ -1,1 +1,11 @@
+-- In stream Analytics under "Job topology" input "goodevents" that has events sorted and sent from function app, output "BeltsAlert" and "goode"
+-- Sends events back to event hub to output "BeltsAlert"
+SELECT * 
+INTO BeltsAlert
+FROM goodevents
+WHERE Buckled = 'NO'
 
+-- Sends all other records to the "goode"
+Select *
+INTO goode
+from goodevents
